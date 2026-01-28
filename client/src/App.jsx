2ae61e5acef014c736e9410eaf0c00226add5d4f
Sara Spano <saraspano@live.com>
2026-01-28 10:45:25 +0100

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import "./i18n";
 
 const App = () => {
     return (
         <>
             <Navbar user={null} />
+            <Footer user={null}/>
         </>
     );
 };
