c088cb15f00fb8dc0cadf0e78b9442040158abca
Sara Spano <saraspano@live.com>
2026-01-28 11:00:48 +0100

    installed react-toastify

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 import "./i18n";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
+    const {darkMode} = useSelector((state) => state.settings);
     return (
         <>
-            <Navbar user={null} />
-            <Footer user={null}/>
+            <Routes>
+                <Route path="/" element ={<Public/>}>
+                </Route>
+            </Routes>
+        
+            <ToastContainer theme={darkMode ? "dark" : "light"} />
         </>
     );
 };
