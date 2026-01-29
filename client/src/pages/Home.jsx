826975f7690f709c4a81f7b004b0ea4784f6507a
Sara Spano <saraspano@live.com>
2026-01-29 17:18:16 +0100

    added new files for homepage

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import spaceHeader from "../assets/spaceHeader.mp4";
+
+const Home = () => {
+  return (
+    <>
+      {/* HEADER */}
+      <header className="">
+
+      </header>
+    </>
+  );
+};
